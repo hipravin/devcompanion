aebbfa43d209cc8532486fee9d8554b1b3f1d1f0
Alex <alexander.vmk@gmail.com>
2022-01-30 19:25:49 +0300

    initial commit, default spring boot module

---
+++ src/main/java/com/hipravin/devcompanion/DevcompanionApplication.java
@@ -0,0 +1,13 @@
+package com.hipravin.devcompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcompanionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevcompanionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hipravin/devcompanion/DevcompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hipravin.devcompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcompanionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
