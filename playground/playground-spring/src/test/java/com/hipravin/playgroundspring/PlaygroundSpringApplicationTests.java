3a884be08ceb59fc36639a7f9928879f1e681186
Alex <alexander.vmk@gmail.com>
2022-02-08 18:59:08 +0300

    - remove stub repo - add few articles - add playground project

---
+++ playground/playground-spring/src/test/java/com/hipravin/playgroundspring/PlaygroundSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hipravin.playgroundspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
