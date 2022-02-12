966cfdcd4de1fdb4b6c368536b2fea11c21f22e8
Alex <alexander.vmk@gmail.com>
2022-02-12 09:51:00 +0300

    - more articles: docker, react

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM alpine:3.14
+RUN rm -rf /usr/local/article-repo
 COPY ./articles-repo /usr/local/article-repo
---
+++ article-service/src/test/java/com/hipravin/devcompanion/article/yml/ArticleYmlMainFileStorageIT.java
@@ -0,0 +1,28 @@
+package com.hipravin.devcompanion.article.yml;
+
+import com.hipravin.devcompanion.article.dto.ArticleDto;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.io.UncheckedIOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled
+class ArticleYmlMainFileStorageIT {
+    final Path articleSampleRepo = Paths.get("../articles-repo");
+
+    @Test
+    void testFindAll() {
+        ArticleYmlFileStorage repository = new ArticleYmlFileStorage(articleSampleRepo);
+
+        List<ArticleDto> articles = repository.loadAll().toList();
+
+        System.out.println("Loaded:" + articles.size());
+    }
+}
