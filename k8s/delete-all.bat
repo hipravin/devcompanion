184dceb5801a8acc47cf73bf789cf33154e11740
Alex <alexander.vmk@gmail.com>
2022-12-16 11:52:33 +0300

    k8s config improvements

--- k8s/delete-all.bat
+++ k8s/delete-all.bat
@@ -1,16 +1,17 @@
-call kubectl delete -f .\admin-service.yml
-call kubectl delete -f .\admin-deployment.yml
+call kubectl delete -f .\admin\admin-service.yml
+call kubectl delete -f .\admin\admin-deployment.yml
 call kubectl delete -f .\devcompanion-ui-service.yml
 call kubectl delete -f .\devcompanion-ui-deployment.yml
 call kubectl delete -f .\gateway-deployment.yml
 call kubectl delete -f .\gateway-service.yml
 call kubectl delete -f .\repo-service.yml
 call kubectl delete -f .\repo-service-deployment.yml
-call kubectl delete -f .\article-service-deployment.yml
-call kubectl delete -f .\article-service.yml
-call kubectl delete -f .\article-repo-pv-claim.yml
-call kubectl delete -f .\article-repo-pv.yml
-call kubectl delete -f .\boot-admin-secret.yml
-call kubectl delete -f .\ingress.yml
-call kubectl delete -f .\cert\devcompanion-tls.yml
+call kubectl delete -f .\article\article-service-deployment.yml
+call kubectl delete -f .\article\article-service.yml
+call kubectl delete -f .\article\article-repo-pv-claim.yml
+call kubectl delete -f .\article\article-repo-pv.yml
+call kubectl delete -f .\secrets\boot-admin-secret.yml
+call kubectl delete -f .\secrets\repo-db-secret.yml
+call kubectl delete -f .\ingress\ingress.yml
+call kubectl delete -f .\ingress\devcompanion-tls.yml
 
